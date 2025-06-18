74b8c3098aad6ddfcf77e709cd7f380da4745a2b
Tjorbenn <tjorben@live.de>
2025-06-18 11:47:44 +0200

    Removed unused variables from main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,1 @@
 import "./style.css";
-
-const searchInput = document.getElementById(
-  "taxonomy-search-input",
-) as HTMLInputElement;
-const resultsContainer = document.getElementById(
-  "autocomplete-results",
-) as HTMLDivElement;
