bd317e94fcb2b11e63ef3e8e0f3d154232a2767f
Tjorbenn <tjorben@live.de>
2025-10-23 13:03:06 +0200

    fixed broken comment

--- compileDocs.sh
+++ compileDocs.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-if $1 do
-  OUT_DIR=$1
-else
-  OUT_DIR="thesis/docs"
-fi
-
-echo "Generating documentation with lits..."
-timeout 10s lits --outDir "$OUT_DIR" || true
-
-echo "Converting markdown to typst..."
 find thesis/docs -type f -name "*.md" -print0 | while IFS= read -r -d $'\0' md_file; do
   typ_file="${md_file%.md}.typ"
   echo "Converting $md_file to $typ_file"
